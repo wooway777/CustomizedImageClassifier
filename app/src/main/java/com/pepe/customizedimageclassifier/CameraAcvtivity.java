73243d4d64035852e174d6814f37321ba3577b29
Wei Wu <wooway777@gmail.com>
2019-08-16 23:59:16 -0400

    crashing version 0

---
+++ app/src/main/java/com/pepe/customizedimageclassifier/CameraAcvtivity.java
@@ -0,0 +1,19 @@
+package com.pepe.customizedimageclassifier;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraAcvtivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
