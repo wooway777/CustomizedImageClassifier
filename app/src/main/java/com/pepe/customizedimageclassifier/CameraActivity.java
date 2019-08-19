264743a6d403cdb3d2858e606c4081d56d2ac4c2
Wei Wu <wooway777@gmail.com>
2019-08-19 15:23:25 -0400

    corrected class name typo

--- app/src/main/java/com/pepe/customizedimageclassifier/CameraActivity.java
+++ app/src/main/java/com/pepe/customizedimageclassifier/CameraActivity.java
@@ -3,7 +3,7 @@ package com.pepe.customizedimageclassifier;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class CameraAcvtivity extends Activity {
+public class CameraActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
